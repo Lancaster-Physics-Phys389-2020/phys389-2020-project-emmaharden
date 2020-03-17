5d61bba9cf9b55761248cc88f6b8592d5e58beeb
emmaharden <61187680+emmaharden@users.noreply.github.com>
2020-03-17 11:15:48 +0000

    Version 2

--- test_file.py
+++ test_file.py
@@ -18,27 +18,33 @@ def test_Lorentz():
     calculated_acc = (0, -5.757108678e8, 0)
     assert math.isclose(Charticle.Lorentz(1.0e-10)[1], calculated_acc[1])
 
-def test_velocity_update():
-    Charticle = Charged_Particle(position = np.array([0.0, 0.0, 0.0], dtype=float), velocity = np.array([0.5, 0.0, 0.0], dtype=float), acceleration = np.array([0.0, 0.0, 0.0], dtype=float), Name = 'Particle', mass = 1.67e-27, charge = 1.6e-19)
-    v = Charticle.update(1e-10)
-    print(v)
-    calculated_v = [0.5, -5.757108678e-2, 0.0]
-    assert math.isclose(v[0], calculated_v[0])
-    assert math.isclose(v[1], calculated_v[1]) #False?
-    assert v[2] == 0
+# def test_velocity_update():
+#     Charticle = Charged_Particle(position = np.array([0.0, 0.0, 0.0], dtype=float), velocity = np.array([0.5, 0.0, 0.0], dtype=float), acceleration = np.array([0.0, 0.0, 0.0], dtype=float), Name = 'Particle', mass = 1.67e-27, charge = 1.6e-19)
+#     acceleration = Charticle.Lorentz(1.0e-10)
+#     v, p = Charticle.update(1.0e-10)
+#     calculated_v = np.array([0.5, -5.757108678e-2, 0.0])
+#     assert np.allclose(v, calculated_v, atol=1e-6)
 
+def test_velocity_update():
+        Charticle = Charged_Particle(position = np.array([0.0, 0.0, 0.0], dtype=float), velocity = np.array([0.5, 0.0, 0.0], dtype=float), acceleration = np.array([0.0, 0.0, 0.0], dtype=float), Name = 'Particle', mass = 1.67e-27, charge = 1.6e-19)
+        acceleration = Charticle.Lorentz(1.0e-10)
+        Charticle.update(1.0e-10)
+        calculated_velocity = np.array([0.5, -5.757108678e-2, 0.0])
+        assert np.allclose(Charticle.velocity, calculated_v, atol=1e-6)
+    
+# def test_position_update():
+#     Charticle = Charged_Particle(position = np.array([0.0, 0.0, 0.0], dtype=float), velocity = np.array([0.5, 0.0, 0.0], dtype=float), acceleration = np.array([0.0, 0.0, 0.0], dtype=float), Name = 'Particle', mass = 1.67e-27, charge = 1.6e-19)
+#     acceleration = Charticle.Lorentz(1.0e-10)
+#     velocity, p = Charticle.update(1.0e-10)
+#     calculated_position = np.array[(0.5e-10, 0.0, 0.0)]
+#     assert np.allclose(p, calculated_position, atol=1e-6)
+    
 def test_position_update():
     Charticle = Charged_Particle(position = np.array([0.0, 0.0, 0.0], dtype=float), velocity = np.array([0.5, 0.0, 0.0], dtype=float), acceleration = np.array([0.0, 0.0, 0.0], dtype=float), Name = 'Particle', mass = 1.67e-27, charge = 1.6e-19)
-    v, p = Charticle.update(1e-10)
-    position = (0.5e-10, 0.0, 0.0) 
-    assert math.isclose(p[0], position[0])
-    assert p[1] == 0
-    assert p[2] == 0
-
-
-
-
-
+    acceleration = Charticle.Lorentz(1.0e-10)
+    Charticle.update(1e-10)
+    calculated_position = np.array[(0.5e-10, 0.0, 0.0)]
+    assert math.isclose(Charticle.position, calculated_position, atol=1e-6)
 
 # def test_KE():
 
